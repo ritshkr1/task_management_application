23f67c14af5b4e82cc2883c2ded9ff0060a2cd2d
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2024-04-16 17:59:42 +0530

    added header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:"add-task",component:AddTaskComponent},{path:"edit-task",component:EditTaskComponent},{path:"",component:TaskListComponent},];
