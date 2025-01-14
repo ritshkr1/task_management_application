44538c43936e2d1177bc44877e4d9cc7949544a1
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2025-01-14 19:16:36 +0530

    added ag grid

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -11,29 +11,67 @@ import { EditorComponent } from '../shared/editor/editor.component';
 import { DeleteModalComponent } from '../shared/delete-modal.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSort, Sort, MatSortable, MatSortModule } from '@angular/material/sort';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 // import {LiveAnnouncer} from '@angular/cdk/a11y';
 import { FormsModule } from '@angular/forms';
 import * as XLSX from 'xlsx';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry, themeQuartz, Theme, } from 'ag-grid-community';
 
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
-
+const myTheme = themeQuartz.withParams({
+  spacing: 15,
+  accentColor: 'red',
+  backgroundColor: 'rgb(249, 245, 227)',
+  foregroundColor: 'rgb(126, 46, 132)',
+  headerTextColor: 'rgb(204, 245, 172)',
+  headerBackgroundColor: 'rgb(209, 64, 129)',
+  oddRowBackgroundColor: 'rgb(0, 0, 0, 0.03)',
+  headerColumnResizeHandleColor: 'rgb(126, 46, 132)',
+});
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatInputModule,MatTableModule, MatSortModule, MatButtonModule, MatPaginatorModule, MatDialogModule, MatSlideToggleModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatTableModule, MatSortModule, MatButtonModule, MatPaginatorModule, MatDialogModule, MatSlideToggleModule, AgGridAngular,],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent implements OnInit, AfterViewInit {
   isDarkTheme: boolean = false;
+  public theme: Theme | "legacy" = myTheme;;
   displayedColumns: string[] = [
     "title",
     "description",
     "status",
     "dueDate",
     "actions"];
+
+  rowData: Task[] = []; // ag-grid row data
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef<Task>[] = [
+    { field: "title" },
+    { field: "description", flex: 2 },
+    { field: "status" },
+    { field: "dueDate" },
+    {
+      headerName: 'Actions',
+      cellRenderer: this.actionCellRenderer.bind(this), // Use a method to render actions
+      width: 200,
+    }
+  ];
+
+  defaultColDef: ColDef = {
+    flex: 1,
+  };
+  // rowSelection ='multiple';
+  pagination = true;
+  paginationPageSize = 5;
+  paginationPageSizeSelector = [5, 10, 20];
   taskList = new MatTableDataSource<Task>([]);
   filteredTasks = new MatTableDataSource<Task>([]);
   filter: string = '';
@@ -58,7 +96,8 @@ export class TaskListComponent implements OnInit, AfterViewInit {
     // Subscribe to the task observable to keep the list updated
     this.taskService.tasks$.subscribe(tasks => {
       this.taskList.data = tasks;
-      this.filteredTasks.data = [...tasks]
+      this.filteredTasks.data = [...tasks];
+      this.rowData = [...this.filteredTasks.data]
       this.filteredTasks.paginator = this.paginator;
     });
   }
@@ -72,7 +111,7 @@ export class TaskListComponent implements OnInit, AfterViewInit {
     );
   }
 
-  
+
 
   applyFilter() {
     const search = this.filter.toLowerCase();
@@ -83,6 +122,7 @@ export class TaskListComponent implements OnInit, AfterViewInit {
         .join('');
       return concatenatedString.includes(search);
     });
+    this.rowData = [...this.filteredTasks.data]
   }
 
   // announceSortChange(sortState: Sort) {
@@ -97,7 +137,30 @@ export class TaskListComponent implements OnInit, AfterViewInit {
   //   }
   // }
 
+  // Renderer method for actions
+  actionCellRenderer(params: any): HTMLElement {
+    const container = document.createElement('div');
+    container.style.display = 'flex';
+    container.style.gap = '10px';
+
+    const editButton = document.createElement('button');
+    editButton.innerHTML = 'Edit';
+    editButton.className = 'mat-flat-button mat-primary';
+    editButton.addEventListener('click', () => this.openEditor(params.data));
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+    deleteButton.className = 'mat-flat-button mat-warn';
+    deleteButton.addEventListener('click', () => this.openDeleteModal(params.data));
+
+    container.appendChild(editButton);
+    container.appendChild(deleteButton);
+
+    return container;
+  }
+
   openEditor(task: any) {
+    console.log(task)
     const dialogRef = this.dialog.open(EditorComponent, {
       width: '70rem',
       data: { task }
